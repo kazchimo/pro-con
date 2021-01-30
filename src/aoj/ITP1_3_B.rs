1f432ac3fb530723391c35a89765d5b64401ad94
kazuki <pharcydetip@gmail.com>
2021-01-30 22:07:12 +0900

    aoj

---
+++ src/aoj/ITP1_3_B.rs
@@ -0,0 +1,13 @@
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
+fn main() {
+    for i in 1..10001 {
+        let v = read::<u16>();
+        if v == 0 { break; }
+        println!("Case {}: {}", i, v)
+    }
+}
