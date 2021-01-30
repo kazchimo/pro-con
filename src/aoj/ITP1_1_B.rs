703f5d72cf9864b206f6dea668039e2279b3c61d
kazuki <pharcydetip@gmail.com>
2021-01-30 20:00:37 +0900

    aoj

---
+++ src/aoj/ITP1_1_B.rs
@@ -0,0 +1,10 @@
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
+fn main() {
+    let n = read::<i32>();
+    println!("{}", n * n * n)
+}
