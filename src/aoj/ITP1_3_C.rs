09b34caf37e59021dd0faac36a3ab0a9bcd7a735
kazuki <pharcydetip@gmail.com>
2021-01-30 22:13:13 +0900

    aoj

---
+++ src/aoj/ITP1_3_C.rs
@@ -0,0 +1,26 @@
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>().split_whitespace()
+        .map(|e| e.parse().ok().unwrap()).collect()
+}
+
+fn main() {
+    loop {
+        let v = read_vec::<u16>();
+
+        if (v[0] == 0) & (v[1] == 0) {
+            break;
+        }
+
+        if v[0] < v[1] {
+            println!("{} {}", v[0], v[1])
+        } else {
+            println!("{} {}", v[1], v[0])
+        }
+    }
+}
