95106e33eda032cb40f3fd7b3df8dea49277bbfb
kazuki <pharcydetip@gmail.com>
2021-01-30 22:23:02 +0900

    aoj

---
+++ src/aoj/ITP1_4_A.rs
@@ -0,0 +1,16 @@
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>().split_whitespace()
+        .map(|e| e.parse().ok().unwrap()).collect()
+}
+
+
+fn main() {
+    let v = read_vec::<u32>();
+    println!("{} {} {}", v[0] / v[1], v[0] % v[1], v[0] as f64 / v[1] as f64)
+}
