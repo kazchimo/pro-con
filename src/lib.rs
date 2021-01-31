4d5c0bd82fd0adca0e8d3b7e1f4d0e11c1cb70e1
kazuki <pharcydetip@gmail.com>
2021-01-31 17:42:43 +0900

    add lib

--- src/lib.rs
+++ src/lib.rs
@@ -26,3 +26,53 @@ fn println_vec<T: Display>(vec: &Vec<T>) {
         }
     }
 }
+
+fn bubble_sort<T: PartialOrd>(vec: &mut Vec<T>) -> &Vec<T> {
+    let mut conti = true;
+
+    while conti {
+        conti = false;
+        for i in (1..vec.len()).rev() {
+            if vec[i] < vec[i - 1] {
+                vec.swap(i, i - 1);
+                conti = true;
+            }
+        }
+    }
+
+    vec
+}
+
+fn selection_sort<T: PartialOrd>(vec: &mut Vec<T>) -> &Vec<T> {
+    for i in 0..vec.len() {
+        let mut minj = i;
+
+        for j in i..vec.len() {
+            if vec[j as usize] < vec[minj as usize] {
+                minj = j
+            }
+        }
+
+        if i != minj {
+            vec.swap(i as usize, minj as usize);
+        }
+    }
+
+    vec
+}
+
+fn insertion_sort<T: PartialOrd + Clone>(vec: &mut Vec<T>) -> &Vec<T> {
+    for i in 1..vec.len() {
+        let v = vec[i].clone();
+        let mut j = i;
+
+        while (j > 0) && (vec[j - 1] > v) {
+            vec[j] = vec[j - 1].clone();
+            j -= 1
+        }
+
+        vec[j] = v;
+    }
+
+    vec
+}
