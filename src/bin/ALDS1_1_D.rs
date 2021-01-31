df0d9051fb0764e0de2dd8b3a927d3dfb5dbbc00
kazuki <pharcydetip@gmail.com>
2021-01-31 15:09:38 +0900

    aoj

---
+++ src/bin/ALDS1_1_D.rs
@@ -0,0 +1,56 @@
+// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_1_D&lang=ja
+
+use std::cmp::max;
+
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
+// require O(n^2) order
+// fn max_diff(r: &i64, rs: Vec<i64>) -> i64 {
+//     rs.iter().fold(std::i64::MIN, |acc, ele| if acc < (ele - r) {
+//         ele - r
+//     } else {
+//         acc
+//     })
+// }
+//
+// fn main() {
+//     let n = read::<usize>();
+//     let mut vec = Vec::with_capacity(n);
+//
+//     for _i in 0..n {
+//         vec.push(read::<i64>());
+//     }
+//
+//     let mut maxes = Vec::with_capacity(n);
+//
+//     for (i, r) in vec.iter().enumerate() {
+//         if i != vec.len() - 1 {
+//             maxes.push(max_diff(r, vec[(i + 1)..vec.len()].to_vec()));
+//         }
+//     }
+//
+//     println!("{}", maxes.iter().fold(&std::i64::MIN, max))
+// }
+
+fn main() {
+    let n = read::<i64>();
+    let mut max = -10_i64.pow(9);
+    let mut min = read::<i64>();
+
+    for _ in 0..n-1 {
+        let r = read::<i64>();
+        if max < (r - min) {
+            max = r - min
+        }
+
+        if r < min {
+            min = r
+        }
+    }
+
+    println!("{}", max)
+}
