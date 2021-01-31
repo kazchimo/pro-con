20ddf14fe86a45aeb16db039d9c9d8b226197071
kazuki <pharcydetip@gmail.com>
2021-01-31 23:57:21 +0900

    aoj

---
+++ src/bin/ALDS1_4_A.rs
@@ -0,0 +1,28 @@
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+
+fn main() {
+    read::<u16>();
+    let s = read_vec::<i32>();
+    read::<u16>();
+    let t = read_vec::<i32>();
+    let mut c = 0;
+
+    for tt in t {
+        if s.contains(&tt) {
+            c = c + 1
+        }
+    }
+
+    println!("{}", c)
+}
